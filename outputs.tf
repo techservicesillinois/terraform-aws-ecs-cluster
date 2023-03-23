72ade7562ea5bb19ec5b9adc6bfcc5d9dd9cf0e7
Jon R. Roma <jonroma@jonroma.net>
2023-03-22 21:26:56 -0500

    Fix bug affecting vpc_id and subnet_id
    This module doesn't need vpc_id and subnet_id for Fargate-only
    clusters. Fix errors that ensue when invoked without those
    input variables, and clean up outputs that don't apply in
    this case.

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,15 @@
-#output "rendered_template" {
-#  value = "${data.template_file.selected.0.rendered}"
-#}
-
-output "security_group_name" {
-  value = aws_security_group.default.*.name
-}
-
-output "security_group_id" {
-  value = aws_security_group.default.*.id
-}
-
 output "name" {
   value = aws_ecs_cluster.default.name
 }
 
 output "id" {
   value = aws_ecs_cluster.default.id
+}
+
+output "security_group_name" {
+  value = (local.ec2) ? aws_security_group.default.*.name : null
+}
+
+output "security_group_id" {
+  value = (local.ec2) ? aws_security_group.default.*.id : null
 }
